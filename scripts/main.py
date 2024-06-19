c9f4d22b5230594642d47e51f1ffa4fecdaf9f9e
Daniel <danielgtravels@gmail.com>
2024-06-19 11:48:20 -0400

    added bloom column and dates

--- scripts/main.py
+++ scripts/main.py
@@ -2,3 +2,4 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+
--- scripts/processors/consolidator.py
+++ scripts/processors/consolidator.py
@@ -1,15 +1,23 @@
 from config import *
 import pandas as pd
 
+
 for group in range(1, 5):
     buoydf = pd.read_csv(f"{DIRECTORY}/data/processed/buoy/{buoyids[group]}_final.txt", sep = ',')
     sstdf = pd.read_csv(f"{DIRECTORY}/data/processed/sst/{sets[group][0]}_{sets[group][1]}.txt", sep = ",")
     riverdf = pd.read_csv(f"{DIRECTORY}/data/processed/river/discharge_{rivergroups[group]}_final.txt", sep = ",")
 
-    df = buoydf.merge(sstdf, on = ['YY', 'MM', 'DD'], how = 'left')
-    df = df.merge(riverdf, on = ['YY', 'MM', 'DD'], how = 'left')
+    df = buoydf.merge(sstdf, on=['YY', 'MM', 'DD'], how='left')
+    df = df.merge(riverdf, on=['YY', 'MM', 'DD'], how='left')
 
-    df.to_csv(f"{DIRECTORY}/data/final/group_{group}.txt", index = False)
+    #adds bloom column
+    df.insert(8, "Bloom", False, True)
+
+    #inserts bloom dates
+    for bloom in blooms[group]:
+        df.loc[(df.YY == bloom[0]) & (df.MM == bloom[1]) & (df.DD == bloom[2]), "Bloom"] = True
+
+    df.to_csv(f"{DIRECTORY}/data/final/group_{group}.txt", index=False)
 
 
 
